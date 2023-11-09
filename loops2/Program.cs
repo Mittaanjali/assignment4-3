4914c66ad546c72569442c45795cd7fed77d275d
Mittaanjali <mitta.anjali@mphasis.com>
2023-11-09 15:32:02 +0530

    loops2

---
+++ loops2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loops2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First 10 Fibonacci Numbers:");
+
+            int n = 10; 
+            int firstNumber = 0;
+            int secondNumber = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(firstNumber + " ");
+
+                int nextNumber = firstNumber + secondNumber;
+                firstNumber = secondNumber;
+                secondNumber = nextNumber;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+    
+
